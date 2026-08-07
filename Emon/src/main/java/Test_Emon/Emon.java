dbd031df646fd06084b0fc7ba42ca92fc65ceeff
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-08 01:19:46 +0600

    Added Emon project

---
+++ Emon/src/main/java/Test_Emon/Emon.java
@@ -0,0 +1,10 @@
+package Test_Emon;
+
+public class Emon {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello Everyone! Kisu Code kori ");
+    }
+}
