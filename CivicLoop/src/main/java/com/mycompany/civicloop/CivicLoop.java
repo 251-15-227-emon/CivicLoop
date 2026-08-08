114b965aa0a63e611d552e7e349855dedfdc6801
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <251-15-796@diu.edu.bd>
2026-08-08 16:02:04 +0600

    Update CivicLoop.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/CivicLoop.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/CivicLoop.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.civicloop;
 
-/**
- *
- * @author Administrator
- */
 public class CivicLoop {
 
     public static void main(String[] args) {
