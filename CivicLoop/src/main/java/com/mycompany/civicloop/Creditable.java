81313326b46fc6f74545e18fbdce0417213d8f73
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-08-09 01:40:03 +0600

    Update Creditable.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/Creditable.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/Creditable.java
@@ -1,16 +1,7 @@
 package civicloop.model;
 
-/**
- * Anything that can be exchanged for TimeCredit (an Item or a Service)
- * implements this interface. Each type decides its own credit rate,
- * which is how the project demonstrates POLYMORPHISM: the same method
- * call (getCreditRate()) behaves differently depending on the actual
- * object (Item vs Service) it is called on.
- */
 public interface Creditable {
-    // How many TimeCredits are earned per hour for this kind of offer.
     double getCreditRate();
 
-    // A short label used in the GUI and in transaction records.
     String getOfferType();
 }
