852d2ca8863d867eaf4d652d75d5c6e115c21be6
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-09 00:44:13 +0600

    Added Emon project
    test

---
+++ Emon/src/main/java/Test/Emon1.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Emon1 {
+    
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println(" Hello Everyone !! Tahole Project suru kora jak");
+    }
+    
+}
