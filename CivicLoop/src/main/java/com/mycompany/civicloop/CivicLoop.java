0d3486f4393972095ca283ab7835b78040198a40
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <251-15-796@diu.edu.bd>
2026-08-08 16:07:36 +0600

    Update CivicLoop.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/CivicLoop.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/CivicLoop.java
@@ -4,6 +4,9 @@ package com.mycompany.civicloop;
 public class CivicLoop {
 
     public static void main(String[] args) {
+        
         System.out.println("Hello World!");
+        
+        
     }
 }
