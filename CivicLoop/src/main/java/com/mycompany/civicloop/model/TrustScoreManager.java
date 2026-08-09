fb2991cb99d47f029e6df353feeb54cea9ad7063
Anika Nishat <251-15-355@diu.edu.bd>
2026-08-10 00:46:15 +0600

    Update TrustScoreManager.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/TrustScoreManager.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/TrustScoreManager.java
@@ -2,6 +2,18 @@ package civicloop.model;
 
 import java.io.Serializable;
 
+/**
+ * I did for :
+ * 
+ * Manages a user's trust score.
+ * SINGLE-RESPONSIBILITY: separated from User so trust rules can change independently.
+ */
+
+public class TrustScoreManager implements Serializable {
+    
 
 
 
+
+    
+}
