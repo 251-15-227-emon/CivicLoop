7b1b6020bbd7531bd500f06ac2af09bfda38ebc4
Anika Nishat <251-15-355@diu.edu.bd>
2026-08-10 00:52:44 +0600

    Update CommunityPost.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/CommunityPost.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/CommunityPost.java
@@ -5,3 +5,20 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
+
+/**
+ * A post on the Community Feed.
+ * Uses a STATIC FACTORY METHOD createPost() instead of a public constructor
+ * to centralise ID/timestamp generation (as required by the UML diagram).
+ */
+
+
+
+
+
+
+
+
+
+
+
