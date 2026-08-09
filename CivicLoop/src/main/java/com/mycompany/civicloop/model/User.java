d550abdbe754f23c21e24febec6c23c6eea87019
251-15-650-Afia <251-15-650@diu.edu.bd>
2026-08-10 00:13:57 +0600

    Update User.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/User.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/User.java
@@ -3,3 +3,10 @@ package civicloop.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.UUID;
+
+/**
+ * Represents a community member.
+ * ENCAPSULATION: password is private – no public getter, only checkPassword().
+ */
+
+
