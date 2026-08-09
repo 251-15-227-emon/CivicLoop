5cdbc579df7ab3c7418be973834b535a7e5b1927
Anika Nishat <251-15-355@diu.edu.bd>
2026-08-09 23:40:54 +0600

    Update Creditable.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/Creditable.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/Creditable.java
@@ -1,3 +1,15 @@
 package civicloop.model;
 
 
+import java.io.Serializable;
+
+public interface Creditable extends Serializable {
+    
+    double getCreditRate();
+
+    String getOfferType();
+}
+
+
+
+
