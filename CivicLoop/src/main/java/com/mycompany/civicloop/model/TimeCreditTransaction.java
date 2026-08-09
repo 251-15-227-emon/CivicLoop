bf6d0b356f2eab571c42cc95ca833bd550244500
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-08-10 00:34:38 +0600

    Update TimeCreditTransaction.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/TimeCreditTransaction.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/TimeCreditTransaction.java
@@ -10,6 +10,19 @@ public class TimeCreditTransaction implements Serializable {
     private double hoursSpent;
     private double creditAmount;
     private String type;         // "Item" or "Service"
+    
+
+
+
+     public TimeCreditTransaction(String fromUserId, String toUserId,
+        double hoursSpent, Creditable offer) {
+        this.transactionId = UUID.randomUUID().toString().substring(0, 8);
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+        this.hoursSpent = hoursSpent;
+        this.creditAmount = calculateCredit(hoursSpent, offer);
+        this.type = offer.getOfferType();
+    }
 
 
 
