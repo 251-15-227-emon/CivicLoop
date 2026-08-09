3b4642ac3514576345a5c2489618d131945c777f
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-08-10 00:34:09 +0600

    Update TimeCreditTransaction.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/TimeCreditTransaction.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/TimeCreditTransaction.java
@@ -2,3 +2,33 @@ package civicloop.model;
 
 import java.io.Serializable;
 import java.util.UUID;
+
+public class TimeCreditTransaction implements Serializable {
+    private String transactionId;
+    private String fromUserId;   // the spender
+    private String toUserId;     // the earner
+    private double hoursSpent;
+    private double creditAmount;
+    private String type;         // "Item" or "Service"
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
