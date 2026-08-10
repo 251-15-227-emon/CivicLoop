0643a818a08dd8b977db2da36b9a07fd08aa3dd0
promit mondal <251-15-214@diu.edu.bd>
2026-08-11 02:07:55 +0600

    Update FeedPanel.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/gui/FeedPanel.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/gui/FeedPanel.java
@@ -4,3 +4,8 @@ import civicloop.model.CommunityPost;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+
+
+/**
+ * Community Feed where users can post messages and like posts.
+ */
