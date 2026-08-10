c7006bc77129aa413faa6ccc7e69a75d35f138c6
Anika Nishat <251-15-355@diu.edu.bd>
2026-08-11 01:34:12 +0600

    Update ItemPanel.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/gui/ItemPanel.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/gui/ItemPanel.java
@@ -5,3 +5,7 @@ import civicloop.model.User;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+
+/**
+ * Panel for adding items and requesting to borrow them.
+ */
