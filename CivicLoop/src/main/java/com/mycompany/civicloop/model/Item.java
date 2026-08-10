4577946c1e39ceb3c7137647cb633cb993dab8bb
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-08-11 02:49:14 +0600

    Update Item.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/Item.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/Item.java
@@ -5,3 +5,27 @@ import java.util.UUID;/**
  * Represents a physical item that can be shared.
  * ENCAPSULATION: all fields private, getters provide controlled access.
  */
+
+
+
+public class Item implements Creditable, Serializable {
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
