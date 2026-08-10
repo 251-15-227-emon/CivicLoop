633fcb1eee00e60f12861e7a1d6c8eb2f915a01a
Anika Nishat <251-15-355@diu.edu.bd>
2026-08-11 01:44:10 +0600

    Update TimeBankPanel.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/gui/TimeBankPanel.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/gui/TimeBankPanel.java
@@ -4,3 +4,7 @@ import civicloop.model.TimeCreditTransaction;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+
+/**
+ * Shows the current user's transaction history and TimeCredit balance.
+ */
