dc59f10e3cb61d50f0eb1d5aa91adb56ab22ef70
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-08-11 01:13:03 +0600

    Update ServicePanel.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/gui/ServicePanel.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/gui/ServicePanel.java
@@ -14,7 +14,29 @@ import java.awt.*;
 public class ServicePanel extends JPanel {
 
 
+private MainFrame parent;
+    private JTable serviceTable;
+    private DefaultTableModel tableModel;
 
+    public ServicePanel(MainFrame parent) {
+        this.parent = parent;
+        setLayout(new BorderLayout());
+
+        tableModel = new DefaultTableModel(new String[]{"ID","Service","Provider","Available"}, 0);
+        serviceTable = new JTable(tableModel);
+        add(new JScrollPane(serviceTable), BorderLayout.CENTER);
+
+        JPanel bottom = new JPanel(new FlowLayout());
+        JButton offerBtn = new JButton("Offer Service");
+        JButton requestBtn = new JButton("Request Selected Service");
+        bottom.add(offerBtn);
+        bottom.add(requestBtn);
+        add(bottom, BorderLayout.SOUTH);
+
+        offerBtn.addActionListener(e -> offerService());
+        requestBtn.addActionListener(e -> requestService());
+        refreshTable();
+    }
 
 
 }
