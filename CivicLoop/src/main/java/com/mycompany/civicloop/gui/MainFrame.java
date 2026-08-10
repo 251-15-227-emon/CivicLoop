58348bcc0939eeb22615863f406a25315c9fe33d
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-11 01:13:29 +0600

    Update MainFrame.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/gui/MainFrame.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/gui/MainFrame.java
@@ -5,3 +5,7 @@ import civicloop.model.User;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+/**
+ * Main dashboard after login.
+ * Contains a JTabbedPane hosting all 5 module panels plus a profile header.
+ */
