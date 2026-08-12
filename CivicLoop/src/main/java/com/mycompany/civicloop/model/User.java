c4eb74aa3bfc09a3dca7380a30ebb1c497fe384a
251-15-650-Afia <251-15-650@diu.edu.bd>
2026-08-12 23:15:39 +0600

    revised

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/User.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/User.java
@@ -4,13 +4,12 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.UUID;
 
-/**
+ /**
  * Represents a community member.
  * ENCAPSULATION: password is private – no public getter, only checkPassword().
  */
 
     public class User implements Serializable {
-
 
     private String userId;
     private String name;
