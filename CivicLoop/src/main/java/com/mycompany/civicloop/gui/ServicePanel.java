f31410fc928a12dd4551af7a8d7eb7be6e344397
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-08-12 23:27:33 +0600

    Update ServicePanel.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/gui/ServicePanel.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/gui/ServicePanel.java
@@ -6,9 +6,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
-
 public class ServicePanel extends JPanel {
-
 
 private MainFrame parent;
     private JTable serviceTable;
