ce5c9f063eeafaf4553077cdaf58e9650291e750
Anika Nishat <251-15-355@diu.edu.bd>
2026-08-13 23:37:53 +0600

    Update TrustScoreManager.java

--- CivicLoop/src/main/java/com/mycompany/civicloop/model/TrustScoreManager.java
+++ CivicLoop/src/main/java/com/mycompany/civicloop/model/TrustScoreManager.java
@@ -3,8 +3,6 @@ package civicloop.model;
 import java.io.Serializable;
 
 /**
- * I did for :
- * 
  * Manages a user's trust score.
  * SINGLE-RESPONSIBILITY: separated from User so trust rules can change independently.
  */
